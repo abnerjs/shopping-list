6a43f7dc11797ed93a605583533dd1d78cd1705f
Abner Silva <abner.js05@gmail.com>
2023-11-14 11:12:00 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import React, { useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="nav"></div>
+    </div>
+  );
+}
+
+export default App;
